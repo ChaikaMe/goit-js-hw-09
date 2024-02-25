710314e964d1bfaedce274203e8cc6f1e4194186
ClownTripleSrank <145134985+ChaikaMe@users.noreply.github.com>
2024-02-25 03:16:56 +0200

    deploy

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const form = document.querySelector(".feedback-form");
+pasteData()
+
+form.addEventListener("input", saveData);
+form.addEventListener("submit", dataSubmit);
+
+function saveData() {
+    const data = {
+        email: form.elements.email.value.trim(),
+        message: form.elements.message.value.trim(),
+    }
+    if (data.email == "" && data.message == "") {
+        localStorage.removeItem('feedback-form-state');
+    }
+    else {
+        localStorage.setItem('feedback-form-state', JSON.stringify(data));
+    }
+}
+
+function pasteData() {
+    try {
+        const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+        form.elements.email.value = data.email;
+        form.elements.message.value = data.message;
+    }
+    catch {return}
+}
+
+function dataSubmit(event) {
+    event.preventDefault();
+    if (form.elements.email.value.trim() == "" &&
+        form.elements.message.value.trim() == "") {
+        alert('Fill the form!')
+    } else {
+        console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+        localStorage.removeItem('feedback-form-state');
+        event.currentTarget.reset();
+    }
+}
